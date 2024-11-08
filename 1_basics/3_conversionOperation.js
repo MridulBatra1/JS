7745ed44fd1d98993d9c091ed606787493cad008
MridulBatra1 <142978984+MridulBatra1@users.noreply.github.com>
2024-11-08 11:14:51 +0000

    conversion Operation

--- 1_basics/3_conversionOperation.js
+++ 1_basics/3_conversionOperation.js
@@ -12,14 +12,63 @@ let isLoggedIN = 1  // "mridul"
 
 let booleanIsLoggedIn = Boolean(isLoggedIN)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let number = 13
-
 let stringNumber = String(number)
 
-console.log(stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(typeof stringNumber);
+
+// ******************* Operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/4);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Mridul"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let counter = 100
+counter++;
+// console.log(counter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
