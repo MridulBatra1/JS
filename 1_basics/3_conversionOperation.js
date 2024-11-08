d478137ca3cc0858052ac8e4bdc208b39ce575ae
MridulBatra1 <142978984+MridulBatra1@users.noreply.github.com>
2024-11-08 10:43:47 +0000

    datatypes conversion

---
+++ 1_basics/3_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIN = 1  // "mridul"
+
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+
+console.log(booleanIsLoggedIn);
+
+let number = 13
+
+let stringNumber = String(number)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
