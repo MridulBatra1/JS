22419b8efabc78e1610f53ddc02b32c92af57ab4
MridulBatra1 <142978984+MridulBatra1@users.noreply.github.com>
2024-11-08 10:13:29 +0000

    datatypes

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "mridul@gmail.com"
+var accountPassword = "12234"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "mb@gmail.com"
+accountPassword = "1232"
+accountCity = "bti"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //new version JS
+
+console.log(3+3);
+
+console.log("MB");
+
+let name = "Mridul"
+let age = 19
+let isLoggedIn = true
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age);
+
+
+
